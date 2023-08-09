d6432eec65bf02f01274966fb5f358c95235ef68
SiddharthKhattar <skhattar111@gmail.com>
2023-08-09 23:22:26 +0530

    Added Multiple Working Pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 import Image from 'next/image'
+import Slider from '@/components/Slider'
+import Featured from '@/components/Featured'
+import Offer from '@/components/Offer'
 
 export default function Home() {
   return (
-    <main> Hello World!</main>
-  )
+    <main>
+      <Slider/>
+      <Featured/>
+      <Offer/>
+    </main>
+
+    )
 }
